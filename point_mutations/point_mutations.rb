ef8f8379661eb83b8bdf90f2473fa44a058865cf
Katherine Ebel <kathyebel@me.com>
2016-05-14 00:23:55 -0400

    removed pry and debug statements

--- point_mutations/point_mutations.rb
+++ point_mutations/point_mutations.rb
@@ -1,18 +1,12 @@
-require 'pry'
-
-class DNA 
+class DNA
   def initialize(strand)
     @strand = strand
   end
 
-  def hamming_distance(other_strand) 
+  def hamming_distance(other_strand)
     return 0 if @strand.eql? other_strand
     @strand.chars.take(other_strand.size).each_with_index.select do |char, index|
-      char != other_strand[index] 
+      char != other_strand[index]
     end.size
   end
 end
-
-strand = 'GACTACGGACAGGGTAGGGAAT'
-distance = 'GACATCGCACACC'
-puts DNA.new(strand).hamming_distance(distance)
