374ddb421f6ccf8ec2bf394ce0f94555d99599f4
Katherine Ebel <kathyebel@me.com>
2016-05-14 00:20:11 -0400

    completed point_mutations challenge

---
+++ point_mutations/point_mutations.rb
@@ -0,0 +1,18 @@
+require 'pry'
+
+class DNA 
+  def initialize(strand)
+    @strand = strand
+  end
+
+  def hamming_distance(other_strand) 
+    return 0 if @strand.eql? other_strand
+    @strand.chars.take(other_strand.size).each_with_index.select do |char, index|
+      char != other_strand[index] 
+    end.size
+  end
+end
+
+strand = 'GACTACGGACAGGGTAGGGAAT'
+distance = 'GACATCGCACACC'
+puts DNA.new(strand).hamming_distance(distance)
